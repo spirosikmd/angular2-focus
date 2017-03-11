2d63d9d00fcc623a3bbda9a813a2b1925fe598b6
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2017-03-11 10:52:27 +0100

    docs(example): use another element and template binding

--- docs/bundle.js
+++ docs/bundle.js
@@ -9,6 +9,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
+        this.isFocused = true;
     }
     return AppComponent;
 }());
